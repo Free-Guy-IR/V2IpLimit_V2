2347dd11b54adc41a093085076c5f881e7bafb11
Free-Guy-IR <115528064+Free-Guy-IR@users.noreply.github.com>
2025-02-08 23:39:53 +0330

    Update v2_ip.sh

--- v2_ip.sh
+++ v2_ip.sh
@@ -1,3 +1,10 @@
-#!binbash
+#!/bin/bash
 
-git clone httpsgithub.comFree-Guy-IRV2IpLimit_V2.git V2IpLimit-Free-Guy-IRV2
+# پوشه مقصد که می‌خواهی فایل‌ها در آن قرار بگیرند
+DEST_DIR="/root/V2IpLimit-Free-Guy-IRV2"
+
+# دانلود مخزن گیت‌هاب و قرار دادن آن در پوشه مقصد
+git clone https://github.com/Free-Guy-IR/V2IpLimit_V2.git "$DEST_DIR"
+
+# نمایش پیام تایید
+echo "مخزن گیت‌هاب در پوشه $DEST_DIR دانلود شد."
