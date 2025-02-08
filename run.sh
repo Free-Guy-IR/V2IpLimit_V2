0383ea6da0a92908bb4cef3bc6ba0d3d6f4e1eaf
Free-Guy-IR <115528064+Free-Guy-IR@users.noreply.github.com>
2025-02-08 23:54:17 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-DEST_DIR="$(pwd)/V2IpLimit-Free-Guy-IRV2"
+# آدرس گیت‌هاب پروژه
+REPO_URL="https://github.com/Free-Guy-IR/V2IpLimit_V2.git"
 
-git clone https://github.com/Free-Guy-IR/V2IpLimit_V2.git "$DEST_DIR"
+# فولدر مقصد
+DEST_DIR="$HOME/V2IpLimit_V2"
 
-echo "Repository cloned to $DEST_DIR"
+# بررسی می‌کنیم که آیا فولدر مقصد موجود است یا نه
+if [ ! -d "$DEST_DIR" ]; then
+    mkdir -p "$DEST_DIR"
+fi
+
+# کلون کردن پروژه به فولدر مقصد
+git clone "$REPO_URL" "$DEST_DIR"
+
+echo "پروژه با موفقیت در فولدر $DEST_DIR دانلود شد."
