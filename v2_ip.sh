37fb90e2249195cb09bfce272f62fb642b191a1f
Free-Guy-IR <115528064+Free-Guy-IR@users.noreply.github.com>
2025-02-08 23:50:03 +0330

    Delete v2_ip.sh

--- v2_ip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# حذف کاراکترهای \r از فایل اسکریپت
-curl -Ls https://raw.githubusercontent.com/Free-Guy-IR/V2IpLimit_V2/main/v2_ip.sh | tr -d '\r' > /tmp/v2_ip_cleaned.sh
-
-# اجرای اسکریپت تمیز شده
-bash /tmp/v2_ip_cleaned.sh
